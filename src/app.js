5e62a939c4f66c625a7ab71a545413c4d490ffb6
Anildo Caldas <anildk5@hotmail.com>
2020-08-10 13:36:56 -0300

    finalized crud with all entities with repository and async requests.

--- src/app.js
+++ src/app.js
@@ -7,7 +7,6 @@ const mongoose = require('mongoose');
 const app = express();
 const router = express.Router();
 
-
 //conectar ao banco NOSql
 mongoose.connect("mongodb+srv://MongoUser:<password>@cursonodejscluster.uywmy.mongodb.net/<dbname>?retryWrites=true&w=majority", {
         useNewUrlParser: true,
@@ -18,21 +17,27 @@ mongoose.connect("mongodb+srv://MongoUser:<password>@cursonodejscluster.uywmy.mo
     })
     .catch(err => {
         console.log('Erro ao conectar com banco de dados: ' + err);
-    })
+    });
+
 
 const Product = require('./models/product');
+const Customer = require('./models/customer');
+const Order = require('./models/order');
 
 //Carregar a Rotas:
 const indexRoute = require('./routes/index-route');
 const productRoute = require('./routes/product-route');
+const customerRoute = require('./routes/customer-route');
+const orderRoute = require('./routes/order-route');
 
 //definindo os midlewares para toda a aplicação:
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: false
 }));
-
 app.use("/", indexRoute);
 app.use("/products", productRoute);
+app.use('/customers', customerRoute);
+app.use('/orders', orderRoute);
 
 module.exports = app;
